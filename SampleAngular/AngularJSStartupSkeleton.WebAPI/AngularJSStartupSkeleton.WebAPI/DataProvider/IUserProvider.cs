98b240564c78c103e6a5a50eb56bd4825757ee7d
mothupally <muralimohan.cse@gmail.com>
2015-02-03 17:50:02 +0530

    Added User model

---
+++ SampleAngular/AngularJSStartupSkeleton.WebAPI/AngularJSStartupSkeleton.WebAPI/DataProvider/IUserProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngularJS.WebAPI.Models;
+namespace AngularJSStartupSkeleton.WebAPI.DataProvider
+{
+    public interface IUserProvider
+    {
+        public bool Add(User user);
+        public bool Delete(User user);
+        public bool Find(User user);
+    }
+}
--- SampleAngular/AngularJSStartupSkeleton.WebAPI/AngularJSStartupSkeleton.WebAPI/Models/User.cs
+++ SampleAngular/AngularJSStartupSkeleton.WebAPI/AngularJSStartupSkeleton.WebAPI/Models/User.cs
@@ -1,6 +1,6 @@
 ﻿namespace AngularJS.WebAPI.Models
 {
-    public class Users
+    public class User
     {
         public string UserName { get; set; }
         public string Password { get; set; }
